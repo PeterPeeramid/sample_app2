4a1fc354eae672575eca39de8b0f281383430737
Peter Morsillo <Peter@peeramid.org>
2011-06-19 18:16:13 -0400

    First commit

---
+++ sample_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6124bc8da6798e50d00fd11d0243e5bf85cc27bcfaec304c28b0429764e6f3a954c0d84c213a6d4dae6e313096a147914fc3e65e8bd7a5bc3255457363a498bb'
