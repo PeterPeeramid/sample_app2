8303e733cc782be82d5a40cbba8e082883799f7e
Peter Morsillo <Peter@peeramid.org>
2011-06-20 18:03:02 -0400

    End of Chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ SampleApp::Application.routes.draw do
   # first created -> highest priority.
 
   # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
+  #   match 'products/:id' => 'cataljkjog#view'
   # Keep in mind you can assign values other than :controller and :action
 
   # Sample of named route:
